ec3a378f3574d19a048402d2cc737b51d42f20ed
ワタナベリョウイチ <146901519+watanabe-ryouichi2005@users.noreply.github.com>
2024-05-20 23:04:22 +0900

    あとmainを書けば完成

---
+++ MT_3_00_01/Calculasion.cpp
@@ -0,0 +1,63 @@
+﻿
+#include "Calculasion.h"
+#include<Novice.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+static const int kColumWidth = 60;
+static const int kRowHeight = 20;
+
+Calculation::Calculation() {
+	v1_ = { 1.0f,3.0f,-5.0f };
+	v2_ = { 4.0f,-1.0f,2.0f };
+	k_ = { 4.0f };
+	resultAdd_ = { Add(v1_, v2_) };
+	resultSubtract_ = { Subtract(v1_, v2_) };
+	resultMultiply_ = { Multiply(k_, v1_) };
+	resultDot_ = { Dot(v1_, v2_) };
+	resultLength_ = { Length(v1_) };
+	resultNormalize_ = { Normalize(v2_) };
+
+
+}
+void Calculation::VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumWidth * 3, y, "%s", label);
+
+
+}
+Vector3 Calculation::Add(const Vector3& v1, const Vector3& v2) {
+	return { v1.x + v2.x,v1.y + v2.y,v1.z + v2.z
+
+	};
+}
+
+Vector3 Calculation::Subtract(const Vector3& v1, const Vector3& v2)
+{
+	return { v1.x - v2.x,v1.y - v2.y,v1.z - v2.z
+
+	};
+}
+Vector3 Calculation::Multiply(float Scaler, const Vector3& v) {
+	return{ Scaler * v.x,Scaler * v.y,Scaler * v.z };
+}
+float Calculation::Dot(const Vector3& v1, const Vector3& v2) {
+	return { v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+	};
+
+}
+float Calculation::Length(const Vector3& v) {
+	float answer = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	return answer;
+}
+Vector3 Calculation::Normalize(const Vector3& v) {
+	float answer = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	Vector3 backnum = { v.x,v.y,v.z };
+	if (answer != 0.0f)
+	{
+		backnum = { v.x / answer,v.y / answer,v.z / answer };
+	}
+	return backnum;
+}
---
+++ MT_3_00_01/Calculasion.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+#include <Vector3.h>
+class Calculation
+{
+
+public://関数
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	Calculation();
+	/// <summary>
+	/// 三次元変換計算関数の出力結果
+	/// </summary>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <param name="vector"></param>
+	/// <param name="label"></param>
+	void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
+
+	/// <summary>
+	/// 加算
+	/// </summary>
+	/// <param name="v1"></param>
+	/// <param name="v2"></param>
+	/// <returns></returns>
+	Vector3 Add(const Vector3& v1, const Vector3& v2);
+	/// <summary>
+	/// 減算
+	/// </summary>
+	/// <param name="v1"></param>
+	/// <param name="v2"></param>
+	/// <returns></returns>
+	Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+
+	/// <summary>
+	/// スカラー倍
+	/// </summary>
+	/// <param name="scalar"></param>
+	/// <param name="v"></param>
+	/// <returns></returns>
+	Vector3 Multiply(float scalar, const Vector3& v);
+
+	/// <summary>
+	/// 内積
+	/// </summary>
+	/// <param name="v1"></param>
+	/// <param name="v2"></param>
+	/// <returns></returns>
+	float Dot(const Vector3& v1, const Vector3& v2);
+
+	/// <summary>
+	/// 長さ(ノルム)
+	/// </summary>
+	/// <param name="v"></param>
+	/// <returns></returns>
+	float Length(const Vector3& v);
+
+	/// <summary>
+	/// 正規化
+	/// </summary>
+	/// <param name="v"></param>
+	/// <returns></returns>
+	Vector3 Normalize(const Vector3& v);
+
+
+private://メンバー変数
+	Vector3 v1_;
+	Vector3 v2_;
+	float k_;
+
+	Vector3 resultAdd_;
+	Vector3 resultSubtract_;
+	Vector3 resultMultiply_;
+	float resultDot_;
+	float resultLength_;
+	Vector3 resultNormalize_;
+};
