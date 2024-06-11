03257bf594d75b6e6b4c6124e15b9c9aaa93f1f9
ワタナベリョウイチ <146901519+watanabe-ryouichi2005@users.noreply.github.com>
2024-06-11 16:04:25 +0900

    kRowheightがいきなり現れたぞ！

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,7 +1,62 @@
+#define USE_MATH_DEFINES
+#include "Matrix4x4.h"
 #include <Novice.h>
+#include <iostream>
+#include <math.h>
 
 const char kWindowTitle[] = "GC1D_ワタナベ_リョウイチ";
 
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	float cotHalfFovV = 1.0f / std::tan(fovY / 2.0f);
+	return {
+	    (cotHalfFovV / aspectRatio),
+	    0.0f,
+	    0.0f,
+	    0.0f,
+	    0.0f,
+	    cotHalfFovV,
+	    0.0f,
+	    0.0f,
+	    0.0f,
+	    0.0f,
+	    farClip / (farClip - nearClip),
+	    1.0f,
+	    0.0f,
+	    0.0f,
+	    -(nearClip * farClip) / (farClip - nearClip),
+	    0.0f};
+}
+Matrix4x4 MakeOrthographicMatrix(
+    float left, float top, float right, float bottom, float nearClip, float farClip) {
+	return {
+		2.0f / (right - left), 0.0f, 0.0f, 0.0f, 0.0f, 2.0f / (top - bottom), 0.0f, 0.0f, 0.0f,
+		    0.0f, 1.0f / (farClip - nearClip), 0.0f, (left+right) / (left-right),
+	    (top+bottom) / (bottom-top),
+	    nearClip / (nearClip-farClip),
+	    1.0f,
+	};
+}
+Matrix4x4 MakeViewportMatrix(
+    float left, float top, float width, float height, float minDepth, float maxDepth) {
+	return {
+	    width / 2.0f,
+	    0.0f,
+	    0.0f,
+	    0.0f,
+	    0.0f,
+	    -height / 2.0f,
+	    0.0f,
+	    0.0f,
+	    0.0f,
+	    0.0f,
+	    maxDepth-minDepth,
+	    0.0f,
+	    left+width / 2.0f,
+	    top+height / 2.0f,
+	    minDepth,
+	    1.0f,
+	};
+}
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -24,6 +79,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+		Matrix4x4 orthographicMatrix =
+		    MakeOrthographicMatrix(-160.f, 160.f, 200.0f, 300.0f, 0.0f, 1000.0f);
+
+		Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 
 		///
 		/// ↑更新処理ここまで
@@ -32,6 +93,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
 
 		///
 		/// ↑描画処理ここまで
