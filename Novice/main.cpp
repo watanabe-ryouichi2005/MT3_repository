7c430dc614d02cde360d3da5849276787eb50cb5
ワタナベリョウイチ <146901519+watanabe-ryouichi2005@users.noreply.github.com>
2024-06-24 14:16:55 +0900

    hint.cpp参考中

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -11,8 +11,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
-	Matrix4x4 m1 = {3.2f,0.7f,9.6f,4.4f,5.5f,
-	1.3f,7.8f}
+	Matrix4x4 m1 = {3.2f, 0.7f, 9.6f, 4.4f, 5.5f, 1.3f, 7.8f, 2.1f,
+	                6.9f, 8.0f, 2.6f, 1.0f, 0.5f, 7.2f, 5.1f, 3.3f};
+	Matrix4x4 m2 = {4.1f, 6.5f, 3.3f, 2.2f, 8.8f, 0.6f, 9.9f, 7.7f,
+	                1.1f, 5.5f, 6.6f, 0.0f, 3.3f, 9.9f, 8.8f, 2.2f};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -26,7 +28,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-
+		Matrix4x4 resultAdd;
 		///
 		/// ↑更新処理ここまで
 		///
@@ -52,3 +54,5 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Finalize();
 	return 0;
 }
+
+
