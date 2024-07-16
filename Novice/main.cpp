b814cc210a1144d56593644fee787d085a05c42c
ワタナベリョウイチ <146901519+watanabe-ryouichi2005@users.noreply.github.com>
2024-07-16 10:26:47 +0900

    inverse関数をつくったMultiply関数でエラー

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,5 +1,6 @@
 #include <Novice.h>
 #include"Matrix4x4.h"
+#include "Vector3.h"
 const char kWindowTitle[] = "GC1D_ワタナベ_リョウイチ";
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -30,7 +31,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		Matrix4x4 resultAdd = Add(m1,m2);
 		Matrix4x4 resultSubtract = Subtract(m1, m2);
-		Matrix4x4 result
+		Matrix4x4 resultmultiply = Multiply(m1, m2);
 
 
 
@@ -108,6 +109,97 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	    return result;
     }
+	const Vector3 Multiply(float scalar,  const Vector3& v) {
+			return { scalar * v.x, scalar * v.y, scalar * v.z};
 
+	}
 
+	Matrix4x4  Inverse(const Matrix4x4& m) {
+		 float determinant = +m.m[0][0] * m.m[1][1] * m.m[2][2] * m.m[3][3]
+        + m.m[0][0] * m.m[1][2] * m.m[2][3] * m.m[3][1]
+        + m.m[0][0] * m.m[1][3] * m.m[2][1] * m.m[3][2]
 
+        - m.m[0][0] * m.m[1][3] * m.m[2][2] * m.m[3][1]
+        - m.m[0][0] * m.m[1][2] * m.m[2][1] * m.m[3][3]
+        - m.m[0][0] * m.m[1][1] * m.m[2][3] * m.m[3][2]
+
+        - m.m[0][1] * m.m[1][0] * m.m[2][2] * m.m[3][3]
+        - m.m[0][2] * m.m[1][0] * m.m[2][3] * m.m[3][1]
+        - m.m[0][3] * m.m[1][0] * m.m[2][1] * m.m[3][2]
+
+        + m.m[0][3] * m.m[1][0] * m.m[2][2] * m.m[3][1]
+        + m.m[0][2] * m.m[1][0] * m.m[2][1] * m.m[3][3]
+        + m.m[0][1] * m.m[1][0] * m.m[2][3] * m.m[3][2]
+
+        + m.m[0][1] * m.m[1][2] * m.m[2][0] * m.m[3][3]
+        + m.m[0][2] * m.m[1][3] * m.m[2][0] * m.m[3][1]
+        + m.m[0][3] * m.m[1][1] * m.m[2][0] * m.m[3][2]
+
+        - m.m[0][3] * m.m[1][2] * m.m[2][0] * m.m[3][1]
+        - m.m[0][2] * m.m[1][1] * m.m[2][0] * m.m[3][3]
+        - m.m[0][1] * m.m[1][3] * m.m[2][0] * m.m[3][2]
+
+        - m.m[0][1] * m.m[1][2] * m.m[2][3] * m.m[3][0]
+        - m.m[0][2] * m.m[1][3] * m.m[2][1] * m.m[3][0]
+        - m.m[0][3] * m.m[1][1] * m.m[2][2] * m.m[3][0]
+
+        + m.m[0][3] * m.m[1][2] * m.m[2][1] * m.m[3][0]
+        + m.m[0][2] * m.m[1][1] * m.m[2][3] * m.m[3][0]
+        + m.m[0][1] * m.m[1][3] * m.m[2][2] * m.m[3][0];
+
+    Matrix4x4 result;
+	float recpDeterminant = 1.0f / determinant;
+    result.m[0][0] = (m.m[1][1] * m.m[2][2] * m.m[3][3] + m.m[1][2] * m.m[2][3] * m.m[3][1] +
+        m.m[1][3] * m.m[2][1] * m.m[3][2] - m.m[1][3] * m.m[2][2] * m.m[3][1] -
+        m.m[1][2] * m.m[2][1] * m.m[3][3] - m.m[1][1] * m.m[2][3] * m.m[3][2]) * recpDeterminant;
+    result.m[0][1] = (-m.m[0][1] * m.m[2][2] * m.m[3][3] - m.m[0][2] * m.m[2][3] * m.m[3][1] -
+        m.m[0][3] * m.m[2][1] * m.m[3][2] + m.m[0][3] * m.m[2][2] * m.m[3][1] +
+        m.m[0][2] * m.m[2][1] * m.m[3][3] + m.m[0][1] * m.m[2][3] * m.m[3][2]) * recpDeterminant;
+    result.m[0][2] = (m.m[0][1] * m.m[1][2] * m.m[3][3] + m.m[0][2] * m.m[1][3] * m.m[3][1] +
+        m.m[0][3] * m.m[1][1] * m.m[3][2] - m.m[0][3] * m.m[1][2] * m.m[3][1] -
+        m.m[0][2] * m.m[1][1] * m.m[3][3] - m.m[0][1] * m.m[1][3] * m.m[3][2]) * recpDeterminant;
+    result.m[0][3] = (-m.m[0][1] * m.m[1][2] * m.m[2][3] - m.m[0][2] * m.m[1][3] * m.m[2][1] -
+        m.m[0][3] * m.m[1][1] * m.m[2][2] + m.m[0][3] * m.m[1][2] * m.m[2][1] +
+        m.m[0][2] * m.m[1][1] * m.m[2][3] + m.m[0][1] * m.m[1][3] * m.m[2][2]) * recpDeterminant;
+
+    result.m[1][0] = (-m.m[1][0] * m.m[2][2] * m.m[3][3] - m.m[1][2] * m.m[2][3] * m.m[3][0] -
+        m.m[1][3] * m.m[2][0] * m.m[3][2] + m.m[1][3] * m.m[2][2] * m.m[3][0] +
+        m.m[1][2] * m.m[2][0] * m.m[3][3] + m.m[1][0] * m.m[2][3] * m.m[3][2]) * recpDeterminant;
+    result.m[1][1] = (m.m[0][0] * m.m[2][2] * m.m[3][3] + m.m[0][2] * m.m[2][3] * m.m[3][0] +
+        m.m[0][3] * m.m[2][0] * m.m[3][2] - m.m[0][3] * m.m[2][2] * m.m[3][0] -
+        m.m[0][2] * m.m[2][0] * m.m[3][3] - m.m[0][0] * m.m[2][3] * m.m[3][2]) * recpDeterminant;
+    result.m[1][2] = (-m.m[0][0] * m.m[1][2] * m.m[3][3] - m.m[0][2] * m.m[1][3] * m.m[3][0] -
+        m.m[0][3] * m.m[1][0] * m.m[3][2] + m.m[0][3] * m.m[1][2] * m.m[3][0] +
+        m.m[0][2] * m.m[1][0] * m.m[3][3] + m.m[0][0] * m.m[1][3] * m.m[3][2]) * recpDeterminant;
+    result.m[1][3] = (m.m[0][0] * m.m[1][2] * m.m[2][3] + m.m[0][2] * m.m[1][3] * m.m[2][0] +
+        m.m[0][3] * m.m[1][0] * m.m[2][2] - m.m[0][3] * m.m[1][2] * m.m[2][0] -
+        m.m[0][2] * m.m[1][0] * m.m[2][3] - m.m[0][0] * m.m[1][3] * m.m[2][2]) * recpDeterminant;
+
+    result.m[2][0] = (m.m[1][0] * m.m[2][1] * m.m[3][3] + m.m[1][1] * m.m[2][3] * m.m[3][0] +
+        m.m[1][3] * m.m[2][0] * m.m[3][1] - m.m[1][3] * m.m[2][1] * m.m[3][0] -
+        m.m[1][1] * m.m[2][0] * m.m[3][3] - m.m[1][0] * m.m[2][3] * m.m[3][1]) * recpDeterminant;
+    result.m[2][1] = (-m.m[0][0] * m.m[2][1] * m.m[3][3] - m.m[0][1] * m.m[2][3] * m.m[3][0] -
+        m.m[0][3] * m.m[2][0] * m.m[3][1] + m.m[0][3] * m.m[2][1] * m.m[3][0] +
+        m.m[0][1] * m.m[2][0] * m.m[3][3] + m.m[0][0] * m.m[2][3] * m.m[3][1]) * recpDeterminant;
+    result.m[2][2] = (m.m[0][0] * m.m[1][1] * m.m[3][3] + m.m[0][1] * m.m[1][3] * m.m[3][0] +
+        m.m[0][3] * m.m[1][0] * m.m[3][1] - m.m[0][3] * m.m[1][1] * m.m[3][0] -
+        m.m[0][1] * m.m[1][0] * m.m[3][3] - m.m[0][0] * m.m[1][3] * m.m[3][1]) * recpDeterminant;
+    result.m[2][3] = (-m.m[0][0] * m.m[1][1] * m.m[2][3] - m.m[0][1] * m.m[1][3] * m.m[2][0] -
+        m.m[0][3] * m.m[1][0] * m.m[2][1] + m.m[0][3] * m.m[1][1] * m.m[2][0] +
+        m.m[0][1] * m.m[1][0] * m.m[2][3] + m.m[0][0] * m.m[1][3] * m.m[2][1]) * recpDeterminant;
+
+    result.m[3][0] = (-m.m[1][0] * m.m[2][1] * m.m[3][2] - m.m[1][1] * m.m[2][2] * m.m[3][0] -
+        m.m[1][2] * m.m[2][0] * m.m[3][1] + m.m[1][2] * m.m[2][1] * m.m[3][0] +
+        m.m[1][1] * m.m[2][0] * m.m[3][2] + m.m[1][0] * m.m[2][2] * m.m[3][1]) * recpDeterminant;
+    result.m[3][1] = (m.m[0][0] * m.m[2][1] * m.m[3][2] + m.m[0][1] * m.m[2][2] * m.m[3][0] +
+        m.m[0][2] * m.m[2][0] * m.m[3][1] - m.m[0][2] * m.m[2][1] * m.m[3][0] -
+        m.m[0][1] * m.m[2][0] * m.m[3][2] - m.m[0][0] * m.m[2][2] * m.m[3][1]) * recpDeterminant;
+    result.m[3][2] = (-m.m[0][0] * m.m[1][1] * m.m[3][2] - m.m[0][1] * m.m[1][2] * m.m[3][0] -
+        m.m[0][2] * m.m[1][0] * m.m[3][1] + m.m[0][2] * m.m[1][1] * m.m[3][0] +
+        m.m[0][1] * m.m[1][0] * m.m[3][2] + m.m[0][0] * m.m[1][2] * m.m[3][1]) * recpDeterminant;
+    result.m[3][3] = (m.m[0][0] * m.m[1][1] * m.m[2][2] + m.m[0][1] * m.m[1][2] * m.m[2][0] +
+        m.m[0][2] * m.m[1][0] * m.m[2][1] - m.m[0][2] * m.m[1][1] * m.m[2][0] -
+        m.m[0][1] * m.m[1][0] * m.m[2][2] - m.m[0][0] * m.m[1][2] * m.m[2][1]) * recpDeterminant;
+
+    return result;
+}
